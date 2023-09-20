618736ce94925c0eb357e3f8aab2585c2ad769a4
nick harrison <500168115@fvtc.edu>
2023-09-19 23:23:03 -0500

    Added Play Button

--- LN7.WebUI/Controllers/QuestionController.cs
+++ LN7.WebUI/Controllers/QuestionController.cs
@@ -17,11 +17,18 @@ namespace LN7.WebUI.Controllers
             _logger = logger;
         }
 
+
         public async Task <IActionResult> Index()
+        {
+            return View();
+        }
+
+        public async Task <IActionResult> DisplayQuestion()
         {
             try
             {
-                GameQuestion question = await GameManager.LoadById(1);
+                int questionState = 1;
+                GameQuestion question = await GameManager.LoadById(questionState);
                 if (question != null)
                 {
                     return View(question);
@@ -37,6 +44,32 @@ namespace LN7.WebUI.Controllers
                 return View("Error");
             }
         }
+
+        public async Task<IActionResult> AnswerQuestion(bool answer, int questionState)
+{
+    try
+    {
+        // Increment the questionState
+        questionState++;
+
+        // Load the next question based on the updated questionState
+        GameQuestion question = await GameManager.LoadById(questionState);
+
+        if (question != null)
+        {
+            return View("Index", question); // Display the next question
+        }
+        else
+        {
+            return View("FinalResult"); // Handle the end of questions
+        }
+    }
+    catch (Exception ex)
+    {
+        // Handle exceptions here
+        return View("Error");
+    }
+}
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
