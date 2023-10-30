de79d6a8d5301233c3fb063daef6195a04188dfd
Boonlert Xiong <300076596@fvtc.edu>
2023-10-30 14:17:45 -0500

    Added API Tests. Rework API Program file

---
+++ LN7.API.Test/UserControllerTest.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LN7.API.Test
+{
+    [TestClass]
+    public class UserControllerTest : BaseTest
+    {
+        [TestMethod]
+        public async Task LoadTestAsync()
+        {
+            await base.LoadTestAsync<User>();
+        }
+
+        [TestMethod]
+        public async Task InsertTestAsync()
+        {
+            User user = new User { First_Name = "Test" };
+            await base.InsertTestAsync<User>(user);
+
+        }
+
+        [TestMethod]
+        public async Task DeleteTestAsync()
+        {
+            await base.DeleteTestAsync<User>(new KeyValuePair<string, string>("First_Name", "Boonlert"));
+        }
+
+        [TestMethod]
+        public async Task LoadByIdTestAsync()
+        {
+            await base.LoadByIdTestAsync<User>(new KeyValuePair<string, string>("First_Name", "Boonlert"));
+        }
+
+        [TestMethod]
+        public async Task UpdateTestAsync()
+        {
+            var key = "First_Name";
+            var value = "Boonlert";
+            User user = null;
+            try
+            {
+                var filter = new KeyValuePair<string, string>(key, value);
+                user = await base.LoadByIdAsync<User>(filter);
+
+                if (user != null)
+                {
+                    user.First_Name = "Test";
+                    user.Last_Name = "Test";
+                    await base.UpdateTestAsync<User>(new KeyValuePair<string, string>("Password", "test"), user);
+                }
+                else
+                {
+                    Assert.IsNotNull(user, $"{key} of {value} not found.");
+                }
+            }
+            catch (Exception)
+            {
+                Debug.WriteLine($"{key} of {value} not found.");
+                Assert.IsNotNull(user, $"{key} of {value} not found.");
+            }
+
+        }
+
+    }
+}
