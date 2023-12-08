f8cdfb70b8090c5d1ae5e9f2bd54826945c9ab22
Boonlert Xiong <300076596@fvtc.edu>
2023-12-07 20:23:03 -0600

    update VM

--- LN7.WebUI/ViewModels/DogVM.cs
+++ LN7.WebUI/ViewModels/DogVM.cs
@@ -7,23 +7,22 @@ namespace LN7.WebUI.ViewModels
     public class DogVM
     {
         public Dog Dog { get; set; }
-        public List <BodyType> BodyTypes { get; set; }
-        public List <CoatColor> CoatColors { get; set; }
-        public List <CoatLength> CoatLengths { get; set; }
-        public List <CoatType> CoatTypes { get; set; }
-        public List <Group> Groups { get; set; }
-        public List <EarLength> EarLengths { get; set; }
-        public List <EarType> EarTypes { get; set; }
-        public List <LegLength> LegLengths { get; set; }    
-        public List <MuzzleLength> MuzzleLengths { get; set; }
-        public List <MuzzleType> MuzzleTypes { get; set; }
-        public List <Origin> OriginTypes { get; set; }
-        public List <TailLength> TailLengths { get; set; }  
-        public List <TailType> TailTypes { get; set; }
-        public List <WeightClass> WeightClasses { get; set; }
+        public List<BodyType> BodyTypes { get; set; }
+        public List<CoatColor> CoatColors { get; set; }
+        public List<CoatLength> CoatLengths { get; set; }
+        public List<CoatType> CoatTypes { get; set; }
+        public List<Group> Groups { get; set; }
+        public List<EarLength> EarLengths { get; set; }
+        public List<EarType> EarTypes { get; set; }
+        public List<LegLength> LegLengths { get; set; }
+        public List<MuzzleLength> MuzzleLengths { get; set; }
+        public List<MuzzleType> MuzzleTypes { get; set; }
+        public List<TailLength> TailLengths { get; set; }
+        public List<TailType> TailTypes { get; set; }
+        public List<WeightClass> WeightClasses { get; set; }
         public IFormFile File { get; set; }
 
-        public DogVM() 
+        public DogVM()
         {
             BodyTypes = DogManager.LoadBody();
             CoatColors = DogManager.CoatColorLoad();
@@ -35,7 +34,6 @@ namespace LN7.WebUI.ViewModels
             LegLengths = DogManager.LegLengthLoad();
             MuzzleLengths = DogManager.MuzzleLengthLoad();
             MuzzleTypes = DogManager.MuzzleTypeLoad();
-            OriginTypes = DogManager.OriginTypeLoad();
             TailLengths = DogManager.TailLengthLoad();
             TailTypes = DogManager.TailTypeLoad();
             WeightClasses = DogManager.WeightClassLoad();
@@ -54,7 +52,6 @@ namespace LN7.WebUI.ViewModels
             LegLengths = DogManager.LegLengthLoad();
             MuzzleLengths = DogManager.MuzzleLengthLoad();
             MuzzleTypes = DogManager.MuzzleTypeLoad();
-            OriginTypes = DogManager.OriginTypeLoad();
             TailLengths = DogManager.TailLengthLoad();
             TailTypes = DogManager.TailTypeLoad();
             WeightClasses = DogManager.WeightClassLoad();
